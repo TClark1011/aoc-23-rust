31f9b417e5172639591e633da288a180648d7b5b
Thomas Clark <thomas.clark@bigtincan.com>
2023-12-01 19:23:39 +1100

    completed day 1 part 1

---
+++ src/bin/01.rs
@@ -0,0 +1,63 @@
+advent_of_code::solution!(1);
+
+pub fn get_first_digit_in_string(input: &str) -> Option<u32> {
+    let mut result: Option<u32> = None;
+    for character in input.chars() {
+        let parsed_digit = character.to_string().parse::<u32>();
+        match parsed_digit {
+            Ok(digit) => {
+                result = Some(digit);
+            }
+            _ => {}
+        };
+        if result != None {
+            break;
+        }
+    }
+
+    result
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let lines = input.lines().collect::<Vec<_>>();
+
+    let numbers_on_each_line: Vec<(u32, u32)> = lines
+        .iter()
+        .map(|&line| {
+            let first_digit = get_first_digit_in_string(line).unwrap();
+
+            let reversed_line_string = line.chars().rev().collect::<String>();
+            let last_digit = get_first_digit_in_string(reversed_line_string.as_str()).unwrap();
+            (first_digit, last_digit)
+        })
+        .collect();
+
+    let sum: u32 = numbers_on_each_line
+        .iter()
+        .fold(0, |sum, (first_digit, last_digit)| {
+            (first_digit * 10) + last_digit + sum
+        });
+
+    Some(sum)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(142));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
