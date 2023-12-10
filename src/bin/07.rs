3fd86156cac5de748a9101fed45c00290bd63a43
Thomas Clark <thomas.clark@bigtincan.com>
2023-12-10 19:16:24 +1100

    scaffold day 7

---
+++ src/bin/07.rs
@@ -0,0 +1,40 @@
+advent_of_code::solution!(7);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    None
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one_example() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_one_actual() {
+        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+        assert!(result.is_some());
+        println!("{}", result.unwrap());
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two_actual() {
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        assert!(result.is_some());
+        println!("{}", result.unwrap());
+    }
+}
