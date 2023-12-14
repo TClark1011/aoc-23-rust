10d3b38e2859caef0b8ea2bb520e843778b3e17a
Thomas Clark <thomas.clark@bigtincan.com>
2023-12-14 14:41:58 +1100

    day 8 part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,10 +1,9 @@
+use num::integer::lcm;
 use std::collections::HashMap;
-
-// use itertools::Itertools;
 
 advent_of_code::solution!(8);
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(PartialEq, Eq, Hash, Copy, Clone)]
 enum NavigationDirection {
     Left,
     Right,
@@ -53,6 +52,7 @@ fn parse_navigation_node_line(line: &str) -> Result<NavigationNode, ParseNavigat
     return Err(ParseNavigationNodeLineError::LineDoesNotMatchPattern);
 }
 
+#[derive(Clone)]
 struct NavigationNodeTraverser {
     active_node_code: String,
     nodes_map: HashMap<String, NavigationNodeBranches>,
@@ -97,8 +97,8 @@ impl NavigationNodeTraverser {
         &mut self,
         directions: Vec<NavigationDirection>,
         destination_code: &str,
-    ) -> u32 {
-        let mut previous_steps: u32 = 0;
+    ) -> u64 {
+        let mut previous_steps: u64 = 0;
 
         while &self.active_node_code != destination_code {
             // println!(
@@ -124,7 +124,7 @@ impl NavigationNodeTraverser {
     }
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<u64> {
     let lines: Vec<&str> = input.lines().collect();
     let directions: Vec<NavigationDirection> = lines[0]
         .chars()
@@ -140,8 +140,61 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(steps)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    /*
+     * Important detail for part 2 that is not mentioned in the task notes: The input is setup
+     * so that if you repeat all the steps you took to get to a `Z` node, you will end up back
+     * at that same node. So we can find how to get all traverser's to a `Z` node by getting each
+     * one to a `Z` node, then getting the lowest common multiple between all of the amount
+     * of steps it took for each.
+     */
+    let lines: Vec<&str> = input.lines().collect();
+    let directions: Vec<NavigationDirection> = lines[0]
+        .chars()
+        .map(parse_navigation_direction_character)
+        .collect();
+
+    let base_traverser = NavigationNodeTraverser::parse_from_input(input);
+
+    let nodes_ending_in_a: Vec<NavigationNode> = lines
+        .iter()
+        .filter_map(|&line| parse_navigation_node_line(line).ok())
+        .filter(|node| node.code.ends_with("A"))
+        .collect();
+
+    let mut traversers: Vec<NavigationNodeTraverser> = nodes_ending_in_a
+        .iter()
+        .map(|node| NavigationNodeTraverser {
+            active_node_code: node.code.to_string(),
+            nodes_map: base_traverser.nodes_map.clone(),
+        })
+        .collect();
+
+    let steps_to_reach_destinations: Vec<u64> = traversers
+        .iter_mut()
+        .map(|traverser| {
+            let mut steps_taken: u64 = 0;
+            while !traverser.active_node_code.ends_with("Z") {
+                let direction_index = usize::try_from(steps_taken).unwrap() % directions.len();
+                traverser.go_direction(&directions[direction_index]);
+                steps_taken += 1;
+            }
+
+            steps_taken
+        })
+        .collect();
+
+    let steps: u64 = steps_to_reach_destinations
+        .iter()
+        .fold(0, |result, &current| {
+            if result == 0 {
+                current
+            } else {
+                lcm(current, result)
+            }
+        });
+
+    Some(steps)
 }
 
 #[cfg(test)]
@@ -164,7 +217,7 @@ mod tests {
     #[test]
     fn test_part_two_example() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 
     #[test]
